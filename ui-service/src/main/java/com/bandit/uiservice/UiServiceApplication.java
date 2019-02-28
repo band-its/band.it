f354ffdd2c220a2a304595702da3c15af584893e
Michal Wiatr <mwiatr.dev@gmail.com>
2019-02-28 20:48:49 +0100

    Added first (partialy working) microservice setup

---
+++ ui-service/src/main/java/com/bandit/uiservice/UiServiceApplication.java
@@ -0,0 +1,17 @@
+package com.bandit.uiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class UiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UiServiceApplication.class, args);
+	}
+
+}
