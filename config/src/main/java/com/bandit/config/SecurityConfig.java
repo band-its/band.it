03ac83164a9d28b99abd96de2d178583ea17df32
Lapek <joi1.512@gmail.com>
2019-03-02 16:21:17 +0100

    backend develop - just refract
    there is no way to run this without docker

--- config/src/main/java/com/bandit/config/SecurityConfig.java
+++ config/src/main/java/com/bandit/config/SecurityConfig.java
@@ -4,9 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-/**
- * @author cdov
- */
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
