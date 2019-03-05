1269cf4d16bd57dfc56a7d1924952b4c4d43b082
Piotr Łapa <joi1.512@gmail.com>
2019-03-05 20:34:19 +0100

    [#164007384] Backend microservice (#4)
    * Added test file
    
    * Added first (partialy working) microservice setup
    
    * Gradlew paralel build
    
    * backend develop - just refract
    
    there is no way to run this without docker
    
    * fix to last commit - simplified root  build.gradle
    
    * travis and jacoco coverage test
    
    also upgrade to java 11
    
    * Fix for gradlew check task. Tests for other classes added. Update to JUnit5.
    
    * Downgrade to JUnit4.
    
    * Added docker-compose config.

---
+++ ui-service/src/test/java/com/bandit/uiservice/UiServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bandit.uiservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class UiServiceApplicationTests {
+    @Test
+    public void contextLoads() {
+    }
+}
