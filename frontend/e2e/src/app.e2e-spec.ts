2a7a34039f00f148df8545fdab50f05a41e68d36
Lapek <joi1.512@gmail.com>
2019-03-10 23:00:38 +0100

    Added e2e task, update tests, update Readme

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to bandit-frontend!');
+    expect(page.getTitleText()).toEqual('Welcome to Band-it!');
   });
 
   afterEach(async () => {
