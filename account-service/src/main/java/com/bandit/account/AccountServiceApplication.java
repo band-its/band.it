682c4ebd19d6cec4d0cacf8c93a390b0a52b1fb5
wiatrM <mwiatr.dev@gmail.com>
2019-03-13 23:04:28 +0100

    Added account service REST endpoint

---
+++ account-service/src/main/java/com/bandit/account/AccountServiceApplication.java
@@ -0,0 +1,24 @@
+package com.bandit.account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableOAuth2Client
+@EnableFeignClients
+@EnableCircuitBreaker
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+}
+
--- account-service/src/test/java/com/bandit/account/AccountServiceApplicationTests.java
+++ account-service/src/test/java/com/bandit/account/AccountServiceApplicationTests.java
@@ -1,15 +1,16 @@
-package com.bandit.accountservice;
+package com.bandit.account;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 public class AccountServiceApplicationTests {
+
     @Test
     public void contextLoads() {
     }
+
 }
