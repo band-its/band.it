75beeb546504b2c1a10930beac90f2184bcac5b6
ssebaa9 <ssebaa9@gmail.com>
2019-03-14 13:58:26 +0100

    Add header and nav

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,27 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavComponent } from './nav/nav.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BandsComponent } from './bands/bands.component';
+import { SongsComponent } from './songs/songs.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomepageComponent,
+    BandsComponent,
+    SongsComponent,
+    SignUpComponent,
+    SignInComponent,
+
   ],
   imports: [
     BrowserModule,
