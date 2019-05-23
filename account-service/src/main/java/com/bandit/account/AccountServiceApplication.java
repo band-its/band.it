1231f6623b0a3b0cab8cf44d637a171998a081c3
Lapek <joi1.512@gmail.com>
2019-05-23 23:48:04 +0200

    Fix for Windows' "too long path" error. Update to SpringBoot to 2.1.5.

--- account-service/src/main/java/com/bandit/account/AccountServiceApplication.java
+++ account-service/src/main/java/com/bandit/account/AccountServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 //import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 //import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.web.servlet.support.RequestContext;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableOAuth2Client
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AccountServiceApplication {
 
     public static void main(String[] args) {
