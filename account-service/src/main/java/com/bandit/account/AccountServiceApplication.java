bb5ebd8eb4e6bfcd3b73d4a2aa11f7e66ac76b30
Piotr Łapa <joi1.512@gmail.com>
2019-10-14 23:03:10 +0200

    2 problems here
    1.  ""Timed out after 30000 ms while waiting to connect. Client view of cluster state is {type=UNKNOWN, servers=[{address=localhost:28017,(...)" for some reason account search for 28017, not 26000
    2. account can't connect to eureka

--- account-service/src/main/java/com/bandit/account/AccountServiceApplication.java
+++ account-service/src/main/java/com/bandit/account/AccountServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-
 
 @SpringBootApplication
 @EnableDiscoveryClient
